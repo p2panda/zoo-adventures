eb37da1e07c1c4f63ce1ec0fb46079469e80dd1d
Vincent Ahrend <mail@vincentahrend.com>
2020-11-13 23:23:52 +0100

    Add experimental output from locally linked Sesamoid

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { set_panic_hook, PandaKeyPair } from 'sesamoid';
+
+set_panic_hook();
+
+const LogWindow = () => {
+  const key_pair = new PandaKeyPair();
+  return <p>Sesamoid says: {key_pair.public_key_bytes()} {key_pair.private_key_bytes()} </p>;
+};
 
 const Home = (): JSX.Element => {
   return (
@@ -30,6 +38,7 @@ const Home = (): JSX.Element => {
         <li>well equiped kitchen</li>
         <li>...</li>
       </ul>
+      <LogWindow />
     </section>
   );
 };

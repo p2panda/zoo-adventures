51c6f214bd271198f233adf75d4d7ea92d7256f6
sandreae <git@contact.samandreae.com>
2021-04-06 00:33:55 +0100

    Remove EntryArgs from PublishEntry

--- src/components/debugview/PublishEntry.tsx
+++ src/components/debugview/PublishEntry.tsx
@@ -19,10 +19,6 @@ const CHAT_SCHEMA =
   '0040cf94f6d605657e90c543b0c919070cdaaf7209c5e1ea58acb8f3568fa2114268dc9ac3bafe12af277d286fce7dc59b7c0c348973c4e9dacbe79485e56ac2a702';
 
 export const PublishEntry = (props) => {
-  const [backlinkHash, setBacklinkHash] = useState<string>();
-  const [skiplinkHash, setSkiplinkHash] = useState<string>();
-  const [lastSeqNum, setLastSeqNum] = useState<number>();
-  const [logId, setLogId] = useState<number>();
   const [entryMessage, setEntryMessage] = useState<string>();
   const [newEntryHashes, setNewEntryHashes] = useState({
     entry: null,
@@ -87,13 +83,6 @@ export const PublishEntry = (props) => {
 
   return (
     <div>
-      <h2>Request next entry arguments</h2>
-      <p>Sends a JSON-RPC call to p2panda node</p>
-      <SyntaxHighlighter>
-        {`const entryArgs = await getNextEntryArgs(publicKey, schema);
-// => { backlinkHash: ${backlinkHash}, \n// skiplinkHash: ${skiplinkHash}, \n// lastSeqNum: ${lastSeqNum}, \n// logId: ${logId} }`}
-      </SyntaxHighlighter>
-
       <h2>Publish Entry</h2>
       <SyntaxHighlighter>{signEncodeSnippet}</SyntaxHighlighter>
       <label>

6de5afa29c80220699a0046dd91bfa3561e57bd5
sandreae <git@contact.samandreae.com>
2021-04-07 23:21:11 +0100

    Prettier formating

--- src/components/BambooLog/LogEntry.tsx
+++ src/components/BambooLog/LogEntry.tsx
@@ -11,33 +11,34 @@ const formatEntryHash = (hash: string): string =>
   )}`;
 
 const formatCheckHash = (hash: string): string => {
-    if (!hash) return 'null';
-    return `#${hash.slice(-4)}`
-}
+  if (!hash) return 'null';
+  return `#${hash.slice(-4)}`;
+};
 
 const formatMessage = (message: string): string => {
-    if (message.length <= 12) return `'${message}'`;
-    return `'${message.slice(-12)}...'`
-}
-  
+  if (message.length <= 12) return `'${message}'`;
+  return `'${message.slice(-12)}...'`;
+};
+
 export const LogEntry = ({ entry }: Props): JSX.Element => {
-    const { message, entryHashBacklink, entryHashSkiplink } = entry.decoded_entry
-    return (
+  const { message, entryHashBacklink, entryHashSkiplink } = entry.decoded_entry;
+  return (
     <div className="flex-column">
-        <div className="entry-data flex-row">
-            <div>{entry.seq_num}</div>
-            <div>{formatEntryHash(entry.entry_hash)}</div>
+      <div className="entry-data flex-row">
+        <div>{entry.seq_num}</div>
+        <div>{formatEntryHash(entry.entry_hash)}</div>
+      </div>
+      <div className="entry-content flex-row">
+        <div className="flex-column">
+          <b>{message.action}</b>
+          <ul>
+            <li>Schema: {formatCheckHash(message.schema)}</li>
+            <li>Message: {formatMessage(message.fields.text.Text)}</li>
+            <li>BackLink: {formatCheckHash(entryHashBacklink)}</li>
+            <li>SkipLink: {formatCheckHash(entryHashSkiplink)}</li>
+          </ul>
         </div>
-        <div className="entry-content flex-row">
-            <div className="flex-column">
-                <b>{message.action}</b>
-                <ul>
-                    <li>Schema: {formatCheckHash(message.schema)}</li>
-                    <li>Message: {formatMessage(message.fields.text.Text)}</li>
-                    <li>BackLink: {formatCheckHash(entryHashBacklink)}</li>
-                    <li>SkipLink: {formatCheckHash(entryHashSkiplink)}</li>
-                </ul>
-            </div>
-        </div>
+      </div>
     </div>
-    )}
+  );
+};

1fa83b7857c63cc1d102f6748ba24e1ff7787b61
Andreas Dzialocha <x12@adz.garden>
2021-05-14 22:08:35 +0200

    Add aquadoggo runtime to Tauri container

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,22 @@
   windows_subsystem = "windows"
 )]
 
-fn main() {
+use aquadoggo::{Configuration, Runtime};
+use async_std::task;
+
+#[async_std::main]
+async fn main() {
+  env_logger::init();
+
+  // Start p2panda node
+  let config = Configuration::new(None).expect("Could not load configuration");
+  let node = Runtime::start(config).await;
+
+  // Show tauri web view
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+
+  // Wait until all tasks are gracefully shut down and exit
+  task::block_on(node.shutdown());
 }

aaac32dba719ef204d703dfae74a79a87a9db31c
Andreas Dzialocha <x12@adz.garden>
2021-12-23 22:45:19 +0100

    Make linter happy

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,11 @@ const App = (): JSX.Element => {
   const syncEntries = async () => {
     const unsortedEntries = await session.queryEntries(CHAT_SCHEMA);
     setEntries(
-      unsortedEntries.sort(({ operation: operationA }, { operation: operationB }) => {
-        return operationA.fields.date > operationB.fields.date ? 1 : -1;
-      }),
+      unsortedEntries.sort(
+        ({ operation: operationA }, { operation: operationB }) => {
+          return operationA.fields.date > operationB.fields.date ? 1 : -1;
+        },
+      ),
     );
   };
 

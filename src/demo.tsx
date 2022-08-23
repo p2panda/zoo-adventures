a1dc4f56861aab2945a75f8b06c7c828819c1951
Andreas Dzialocha <x12@adz.garden>
2022-08-23 18:10:51 +0200

    Initialise key pair and pass over configuration

--- src/demo.tsx
+++ src/demo.tsx
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom/client';
 
 import { ZooAdventures } from './';
 
+const ENDPOINT = 'http://localhost:2020/graphql';
+const SCHEMA_ID =
+  'zoo_adventures_00209403784eea2ba5236f9083472f040a90babf4df1b64efbbaae58eb51aa67fd4d';
+const DOCUMENT_ID =
+  '0020c301d6595e12cbd472d21ac4c1bdfde02ec4e8d7ea299f723130d64eb65e500d';
+
 const rootElem = document.createElement('div');
 document.body.appendChild(rootElem);
 
 const root = ReactDOM.createRoot(rootElem);
-root.render(<ZooAdventures />);
+root.render(
+  <ZooAdventures
+    endpoint={ENDPOINT}
+    schemaId={SCHEMA_ID}
+    documentId={DOCUMENT_ID}
+  />,
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { GraphQLClient, gql } from 'graphql-request';
+import { initWebAssembly, KeyPair } from 'p2panda-js';
 
 import type { FunctionComponent } from 'react';
 
-export const ZooAdventures: FunctionComponent = () => {
-  return <h1>Hello, Panda!</h1>;
+const ENDPOINT = 'http://localhost:2020/graphql';
+const PRIVATE_KEY_STORE = 'privateKey';
+
+const GQL_NEXT_ARGS = gql`
+  query NextArgs($publicKey: String!, $viewId: String) {
+    nextArgs(publicKey: $publicKey, viewId: $viewId) {
+      logId
+      seqNum
+      backlink
+      skiplink
+    }
+  }
+`;
+
+const client = new GraphQLClient(ENDPOINT, {
+  mode: 'cors',
+});
+
+async function nextArgs(publicKey: string, viewId?: string) {
+  const result = await client.request(GQL_NEXT_ARGS, {
+    publicKey,
+    viewId,
+  });
+
+  return result.data;
+}
+
+function initialiseKeyPair(): KeyPair {
+  const privateKey = window.localStorage.getItem(PRIVATE_KEY_STORE);
+  if (privateKey) {
+    return new KeyPair(privateKey);
+  }
+
+  const keyPair = new KeyPair();
+  window.localStorage.setItem(PRIVATE_KEY_STORE, keyPair.privateKey());
+  return keyPair;
+}
+
+type GameProps = {
+  keyPair: KeyPair;
+  config: Configuration;
+};
+
+const Game: FunctionComponent<GameProps> = ({ keyPair, config }) => {
+  useEffect(() => {
+    const init = async () => {
+      const test = await nextArgs(keyPair.publicKey(), config.documentId);
+      console.log(test);
+    };
+
+    init();
+  }, []);
+
+  return <p>{keyPair.publicKey()}</p>;
+};
+
+export type Configuration = {
+  endpoint: string;
+  schemaId: string;
+  documentId: string;
+};
+
+export const ZooAdventures: FunctionComponent<Configuration> = (config) => {
+  const [keyPair, setKeyPair] = useState<KeyPair>();
+
+  useEffect(() => {
+    const init = async () => {
+      await initWebAssembly();
+      setKeyPair(initialiseKeyPair());
+    };
+
+    init();
+  }, []);
+
+  return keyPair ? <Game keyPair={keyPair} config={config} /> : null;
 };

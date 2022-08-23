921e1f0b2534c7a95bff49fb0ad23b3cc4313e49
Andreas Dzialocha <x12@adz.garden>
2022-08-23 13:10:23 +0200

    Delete everything

--- src/configs.ts
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-export const ENDPOINT = 'http://localhost:2020';
-export const CHAT_SCHEMA =
-  '00200cf84048b0798942deba7b1b9fcd77ca72876643bd3fedfe612d4c6fb60436be';

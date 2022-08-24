cee7e0b8541ff0158263924cc9b1c49999fad381
Andreas Dzialocha <x12@adz.garden>
2022-08-24 11:33:53 +0200

    Change winSize to 3, check for sane configs

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import type { Configuration } from './types';
 
 const defaultConfiguration: Configuration = {
   boardSize: 4,
-  winSize: 4,
+  winSize: 3,
   documentId:
     '0020c301d6595e12cbd472d21ac4c1bdfde02ec4e8d7ea299f723130d64eb65e500d',
   endpoint: 'http://localhost:2020/graphql',
@@ -16,10 +16,29 @@ const defaultConfiguration: Configuration = {
   updateIntervalMs: 2000,
 };
 
-export const ZooAdventures: React.FC<Partial<Configuration>> = (config) => {
+export const ZooAdventures: React.FC<Partial<Configuration>> = (
+  userConfiguration,
+) => {
+  const config = {
+    ...defaultConfiguration,
+    ...userConfiguration,
+  };
+
+  if (config.boardSize < 3) {
+    throw new Error('boardSize can not be smaller than 2');
+  }
+
+  if (config.winSize < 2) {
+    throw new Error('winSize can not be smaller than 1');
+  }
+
+  if (config.boardSize < config.winSize) {
+    throw new Error('winSize can not be larger than boardSize');
+  }
+
   return (
     <InitWasm>
-      <Game config={{ ...defaultConfiguration, ...config }} />
+      <Game config={config} />
     </InitWasm>
   );
 };
